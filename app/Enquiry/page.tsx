81e3f78c0bfeae1089e1343428fc689ab57a0c59
KAVYAJAIN410 <kavya.jain2023@vitstudent.ac.in>
2025-05-29 17:49:03 +0530

    fj

--- app/Enquiry/page.tsx
+++ app/Enquiry/page.tsx
@@ -5,7 +5,8 @@ export default function Enquiry() {
       <h1 className='m-7'>
           <span className="text-[#0a2240] text-3xl m-4" style={{fontWeight:"bolder"}}>CONTACT DETAILS</span> 
         </h1>
-        <div className="flex">
+        <div></div>
+        <div className="md:flex">
         <div>
         <p className=" text-lg  leading-tight mt-4 pl-10 font-bold">Dr. A Nirmala Grace</p>
         <p className=" text-lg  leading-tight  pl-10">Director, Organising Chair</p>
