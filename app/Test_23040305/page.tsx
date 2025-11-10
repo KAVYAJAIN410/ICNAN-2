f9813dfba8ba54e5844eef699f2942587c349bb1
KAVYAJAIN410 <kavya.jain2023@vitstudent.ac.in>
2025-11-10 15:51:23 +0530

    o

---
+++ app/Test_23040305/page.tsx
@@ -0,0 +1,19 @@
+export default function App(){
+return(
+    <div className="h-[200vh] relative">
+        <div className="absolute bottom-10 left-20">
+        <button >
+       <a
+  href="/CONF.docx"
+  download
+>
+ <div className="h-2 w-2 bg-gray-500">
+
+ </div>
+</a>
+
+      </button>
+      </div>
+    </div>
+)
+}
